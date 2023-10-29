5ec8437cd97981c1f3653bb11826e463175af9ce
Abdurashid Abdumominov <78313030+abdurashid-dev@users.noreply.github.com>
2023-10-29 10:43:37 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,83 +10,83 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-//    //Problem 1
-//    int n;
-//    cout << "Enter a number (n): ";
-//    cin >> n;
-//
-//    cout << "Odd numbers: ";
-//    for (int i = 1; i <= n; i++) {
-//        if (i % 2 != 0) {
-//            cout << i <<' ';
-//        }
-//    }
-//
-//    cout << "\nEven numbers: ";
-//    for (int i = 1; i <= n; i++) {
-//        if (i % 2 == 0) {
-//            cout << i <<' ';
-//        }
-//    }
+   //Problem 1
+   int n;
+   cout << "Enter a number (n): ";
+   cin >> n;
+
+   cout << "Odd numbers: ";
+   for (int i = 1; i <= n; i++) {
+       if (i % 2 != 0) {
+           cout << i <<' ';
+       }
+   }
+
+   cout << "\nEven numbers: ";
+   for (int i = 1; i <= n; i++) {
+       if (i % 2 == 0) {
+           cout << i <<' ';
+       }
+   }
     
-    //Problem 2
-//    int n;
-//    cout << "Enter a number (n): ";
-//    cin >> n;
-//
-//    int sumOdd = 0;
-//    int sumEven = 0;
-//
-//    for (int i = 1; i <= n; i++) {
-//        if (i % 2 != 0) {
-//            sumOdd += i;
-//        } else {
-//            sumEven += i;
-//        }
-//    }
-//
-//    cout << "Sum of odd numbers: " << sumOdd << endl;
-//    cout << "Sum of even numbers: " << sumEven << endl;
+    Problem 2
+   int n;
+   cout << "Enter a number (n): ";
+   cin >> n;
+
+   int sumOdd = 0;
+   int sumEven = 0;
+
+   for (int i = 1; i <= n; i++) {
+       if (i % 2 != 0) {
+           sumOdd += i;
+       } else {
+           sumEven += i;
+       }
+   }
+
+   cout << "Sum of odd numbers: " << sumOdd << endl;
+   cout << "Sum of even numbers: " << sumEven << endl;
     
-//    //Problem 3
-//    int n;
-//    cout << "Enter the number of Fibonacci numbers to generate (N):";
-//    cin >> n;
-//
-//    int first = 0;
-//    int second = 1;
-//    int sum = 0;
-//
-//    cout << "Fibonacci sequence: ";
-//    for (int i = 0; i < n; i++) {
-//        cout << first << " ";
-//        sum += first;
-//        int next = first + second;
-//        first = second;
-//        second = next;
-//    }
-//
-//    cout << "\nSum of Fibonacci numbers: " << sum;
+   //Problem 3
+   int n;
+   cout << "Enter the number of Fibonacci numbers to generate (N):";
+   cin >> n;
+
+   int first = 0;
+   int second = 1;
+   int sum = 0;
+
+   cout << "Fibonacci sequence: ";
+   for (int i = 0; i < n; i++) {
+       cout << first << " ";
+       sum += first;
+       int next = first + second;
+       first = second;
+       second = next;
+   }
+
+   cout << "\nSum of Fibonacci numbers: " << sum;
     
-//    //Problem 4
-//    int number;
-//    cout << "Enter an integer: ";
-//    cin >> number;
-//
-//    int sum = 0;
-//
-//    while (number != 0) {
-//        sum += number % 10;
-//        number /= 10;
-//    }
-//
-//    cout << "Sum of digits: " << sum << endl;
-//
-//    if (sum % 5 == 0) {
-//        cout << "Yes";
-//    } else {
-//        cout << "No";
-//    }
+   //Problem 4
+   int number;
+   cout << "Enter an integer: ";
+   cin >> number;
+
+   int sum = 0;
+
+   while (number != 0) {
+       sum += number % 10;
+       number /= 10;
+   }
+
+   cout << "Sum of digits: " << sum << endl;
+
+   if (sum % 5 == 0) {
+       cout << "Yes";
+   } else {
+       cout << "No";
+   }
     
     //Problem 5
     int n;
